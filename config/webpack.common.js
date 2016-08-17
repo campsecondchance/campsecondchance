c213ea7b060db957832ef7d7e90e42137007a2cb
Hunter Hunnie Bastard <scrapcupcake@gmail.com>
2016-08-17 10:13:19 -0700

    Fixed styling and presentation issues

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -18,7 +18,7 @@ const HtmlElementsPlugin = require('./html-elements-plugin');
  * Webpack Constants
  */
 const METADATA = {
-  title: 'Angular2 Webpack Starter by @gdi2290 from @AngularClass',
+  title: 'Camp Second Chance: A clean and sober encampment',
   baseUrl: '/',
   isDevServer: helpers.isWebpackDevServer()
 };
